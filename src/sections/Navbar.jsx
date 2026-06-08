10e6f0e3f427bcb38a23c8045a1de4e2aa590008
grootste <singh.baibhav321@gmail.com>
2026-06-09 00:21:42 +0545

    first com

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      Navbar
+    </header>
+  )
+}
+
+export default Navbar
