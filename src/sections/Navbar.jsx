e2a2eeafc338c7d97585afe9e68ce8d209224121
grootste <singh.baibhav321@gmail.com>
2026-06-09 00:22:06 +0545

    navvar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
-      Navbar
+      div className="max-w-7xl mx-auto flex items-center justify-between h-16 px-4">
     </header>
   )
 }
